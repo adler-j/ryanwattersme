45f76c620712bc97aded7d0eb5c369b357f33354
Ryan Watters <watters.ryan@gmail.com>
2015-08-29 14:17:37 -0500

    Addition of TypeKit fonts - playing with more styles

--- scripts/tipuesearch_content.js
+++ scripts/tipuesearch_content.js
@@ -5,12 +5,12 @@ insearch: false
 var tipuesearch = {"pages": [
 		{% for page in site.pages %}
 		{% if page.insearch != false or page.insearch == nil %}
-		{"title": "{{page.title}}","text": {{page.content | jsonify }}, "tags": "{% if page.tags %}{% for tag in page.tags %}{{tag}}{% if forloop.rindex0 > 0 %}, {% endif %}{% endfor %}{% endif %}","url":"{{page.url}}"},
+		{"title": "{{page.title}}","text": {{page.content | jsonify | strip_html }}, "tags": "{% if page.tags %}{% for tag in page.tags %}{{tag}}{% if forloop.rindex0 > 0 %}, {% endif %}{% endfor %}{% endif %}","url":"{{page.url}}"},
 		{% endif %}
 		{% endfor %}
 		{% for article in site.collections.articles.docs %}
 		{% if article.insearch != false or page.insearch == nil %}
-		{"title": "{{article.title}}", "text": {{article.content | jsonify }}, "tags": "{% if article.tags %}{% for tag in article.tags %}{{tag}}{% if forloop.rindex0 > 0 %}, {% endif %}{% endfor %}{% endif %}", "url": "{{article.url}}"}{% if forloop.rindex0 > 0 %},{% endif %}
+		{"title": "{{article.title}}", "text": {{article.content | jsonify | strip_html }}, "tags": "{% if article.tags %}{% for tag in article.tags %}{{tag}}{% if forloop.rindex0 > 0 %}, {% endif %}{% endfor %}{% endif %}", "url": "{{article.url}}"}{% if forloop.rindex0 > 0 %},{% endif %}
 		{% endif %}
 		{% endfor %}
 
