60d10780763777603a75db03925efa01eddd8378
Ryan Watters <watters.ryan@gmail.com>
2015-09-02 11:07:02 -0500

    Inlined CSS to head of page

--- _site/scripts/tipuesearch_content.js
+++ _site/scripts/tipuesearch_content.js
@@ -26,8 +26,6 @@ var tipuesearch = {"pages": [
 		
 	
 		
-	
-		
   		
       	
   		
--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,14 @@ $('iframe').wrap('<section class="article-video"></section>');
 
 $(document).ready(function() {
     //Add animate.css class to hero/header/site-intro
-    $('.site-title-container').addClass('fadeIn');
-    //mobile navigation toggling
-    $('#mobile-toggle').on('click', function() {
-        if (window.outerWidth < 641) {
-            $('.global-navigation').toggleClass('mobile-menu');
-            $('#mobile-toggle').toggleClass('active');
-        }
-    });
+    // $('.site-title-container').addClass('fadeIn');
+    // //mobile navigation toggling
+    // $('#mobile-toggle').on('click', function() {
+    //     if (window.outerWidth < 641) {
+    //         $('.global-navigation').toggleClass('mobile-menu');
+    //         $('#mobile-toggle').toggleClass('active');
+    //     }
+    // });
     //Add current year to copyright in footer
     var date = new Date();
     var thisYear = date.getFullYear();
