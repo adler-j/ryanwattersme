b34d5cc75bcb27c292df031f24efb5c90b09913d
Ryan Watters <watters.ryan@gmail.com>
2015-09-02 11:29:24 -0500

    Fixes to js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,6 @@
 $('iframe').wrap('<section class="article-video"></section>');
 
 $(document).ready(function() {
-    //Add animate.css class to hero/header/site-intro
-    // $('.site-title-container').addClass('fadeIn');
-    // //mobile navigation toggling
-    // $('#mobile-toggle').on('click', function() {
-    //     if (window.outerWidth < 641) {
-    //         $('.global-navigation').toggleClass('mobile-menu');
-    //         $('#mobile-toggle').toggleClass('active');
-    //     }
-    // });
     //Add current year to copyright in footer
     var date = new Date();
     var thisYear = date.getFullYear();
