14eb5dad3ff9458a58c819c16cbe931a334c9da6
Ryan Watters <watters.ryan@gmail.com>
2015-09-13 17:48:23 -0500

    Cacheable assets moved to "assets" folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
-            options: {
-                banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
-            },
             build: {
                 files: {
                     '_includes/minified.min.js': ['gruntscripts/*.js']
