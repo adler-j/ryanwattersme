bdd18d68656585775fcf43e8bd2722da132d8cd7
Ryan Watters <watters.ryan@gmail.com>
2015-09-14 12:59:53 -0500

    Added experimental pushState script to main.js

--- gruntscripts/main.js
+++ gruntscripts/main.js
@@ -62,24 +62,50 @@ function toggleSearch() {
 function preventRefresh(enterTerm) {
     enterTerm.preventDefault();
 }
+//PushState and jQuery to use AJAX to update #main content
+//Taken from https://rosspenman.com/pushstate-jquery/
 
-$(document).ready(function() {
-    $('.tag-ajax').on('click', function() {
-        $.ajax({
-            url: '../assets/scripts/tags.json',
-            dataType: 'json',
-            type: 'GET',
-            cache: false,
-            success: function(data) {
-                var theList = $('.matching-items');
-                var theItems = [];
-                $(data.siteContent).each(function(index, value) {
-                    theItems.push("'<li>' + ");
-                });
-            }
-        });
-    });
-});
+$(function() {
+  String.prototype.decodeHTML = function() {
+    return $("<div>", {html: "" + this}).html();
+  };
 
+  var $main = $("#main"),
+  
+  init = function() {
+    hljs.initHighlightingOnLoad();
+  },
+  
+  ajaxLoad = function(html) {
+    document.title = html
+      .match(/<title>(.*?)<\/title>/)[1]
+      .trim()
+      .decodeHTML();
 
-console.log("Yo!");
+    init();
+  },
+  
+  loadPage = function(href) {
+    $main.load(href + " main>*", ajaxLoad);
+  };
+  
+  init();
+  
+  $(window).on("popstate", function(e) {
+    if (e.originalEvent.state !== null) {
+      loadPage(location.href);
+    }
+  });
+
+  $(document).on("click", "a, area", function() {
+    var href = $(this).attr("href");
+
+    if (href.indexOf(document.domain) > -1
+      || href.indexOf(':') === -1)
+    {
+      history.pushState({}, '', href);
+      loadPage(href);
+      return false;
+    }
+  });
+})
