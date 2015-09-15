62591baaa530697ed3c3ed5e753d7d4047bbda4e
Ryan Watters <watters.ryan@gmail.com>
2015-09-14 19:00:36 -0500

    Fixed issue with article body extending too far down on mobile for FF

--- gruntscripts/main.js
+++ gruntscripts/main.js
@@ -46,7 +46,7 @@ function targetBlank() {
 function toggleSearch() {
     var searchForm = document.querySelector("#search-form"),
         searchInput = document.getElementById('tipue_search_input'),
-        searchFooter = document.getElementById('footer');
+        searchFooter = document.getElementById('site-footer');
     searchForm.classList.toggle('search-open');
     searchInput.classList.toggle('search-open');
     searchFooter.classList.toggle('search-open');
@@ -62,55 +62,3 @@ function toggleSearch() {
 function preventRefresh(enterTerm) {
     enterTerm.preventDefault();
 }
-//PushState and jQuery to use AJAX to update #main content
-//Taken from https://rosspenman.com/pushstate-jquery/
-
-$(function() {
-    String.prototype.decodeHTML = function() {
-        return $("<div>", {
-            html: "" + this
-        }).html();
-    };
-
-    var $main = $("#main"),
-
-        rebootScripts = function() {
-            hljs.initHighlightingOnLoad();
-            disQus();
-            fluidvids.init({
-                selector: ['iframe'], // runs querySelectorAll()
-                players: ['www.youtube.com', 'player.vimeo.com'] // players to support
-            });
-        },
-
-        ajaxLoad = function(html) {
-            document.title = html
-                .match(/<title>(.*?)<\/title>/)[1]
-                .trim()
-                .decodeHTML();
-
-            rebootScripts();
-        },
-
-        loadPage = function(href) {
-            $main.load(href + " main>*", ajaxLoad);
-        };
-
-    rebootScripts();
-
-    $(window).on("popstate", function(e) {
-        if (e.originalEvent.state !== null) {
-            loadPage(location.href);
-        }
-    });
-
-    $(document).on("click", "a, area", function() {
-        var href = $(this).attr("href");
-
-        if (href.indexOf(document.domain) > -1 || href.indexOf(':') === -1) {
-            history.pushState({}, '', href);
-            loadPage(href);
-            return false;
-        }
-    });
-})
