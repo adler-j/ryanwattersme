3043be7291e14a8bacd84aa49816f4119af283aa
Ryan Watters <watters.ryan@gmail.com>
2015-09-15 17:58:38 -0500

    Improvements to tag page
    Removed images as background, rewrote jsonTagLoading.js so that it no
    longer needs jQuery as a dependency.

--- gruntscripts/jsonTagLoading.js
+++ gruntscripts/jsonTagLoading.js
@@ -1,37 +1,21 @@
-// var tagId = '';
-// $('.tag-ajax').on('click', function(event) {
-//     tagId = event.target.id;
-//     var request = new XMLHttpRequest;
-//     request.open('GET', '../assets/scripts/tags.json', true);
-//     request.onreadystatechange = function() {
-//         if ((request.status === 200) && (request.readyState === 4)) {
-//             var rawContent = JSON.parse(request.responseText),
-//                 content = rawContent.siteContent,
-//                 matchingItems = document.getElementById('matching-items');
-//             $('.matching-items > li').fadeOut();
-//             matchingItems.innerHTML = '';
-//             for (var i = 0; i < content.length; i++) {
-//                 if (content[i].articleTag == tagId) {
-//                     matchingItems.innerHTML += '<li class="json-tag-match animated fadeInUp"><a href=\"' + content[i].articleUrl + '\">' + '<section style=\"background-image:url(/assets/images/' + content[i].articleImage + ')\"><h5>' + content[i].articleTitle + '</h5><p>' + content[i].articleDescription + '</p></section></li>';
-//                 }
-//             }
-//         }
-//     }
-//     request.send();
-// });
+document.addEventListener('DOMContentLoaded',checkHash(),false);
+window.addEventListener('click', function(event) {
+				event.preventDefault();
+    if (event.target.classList.contains("tag-ajax")) {
+        var clickedTag = event.target.id;
+        getMatches(clickedTag);
 
+    }
+}, false);
 
-$(document).ready(function() {
+function checkHash() {
     if (window.location.hash.length !== 0) {
         theTag = window.location.hash.split('#')[1];
+        theTagText = theTag.replace('-',' ');
+        console.log(theTagText);
         getMatches(theTag);
     }
-});
-
-$('.tag-ajax').on('click', function(event) {
-    var clickedTag = event.target.id;
-    getMatches(clickedTag);
-});
+}
 
 function getMatches(el) {
     var tagId = el;
@@ -45,12 +29,15 @@ function getMatches(el) {
             $('.matching-items > li').fadeOut();
             matchingItems.innerHTML = '';
             for (var i = 0; i < content.length; i++) {
-                if (content[i].articleTag == tagId) {
-                    matchingItems.innerHTML += '<li class="json-tag-match animated fadeInUp"><a  style=\"background-image:linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url(/assets/images/' + content[i].articleImage + ')\" href=\"' + content[i].articleUrl + '\">' + '<section><h5>' + content[i].articleTitle + '</h5><p>' + content[i].articleDescription + '</p></section></li>';
+                if (content[i].tag == tagId) {
+                  matchingItems.innerHTML += '<li class=\"animated fadeInUp\"><a href=\"' + content[i].url + '\"><img src=\"/assets/images/' + content[i].image + '\"/><section><h5>' + content[i].title + '</h5><p>' + content[i].description + '</p></section></a></li>';
                 }
             }
         }
     }
     request.send();
-
 }
+
+// if (content[i].articleTag == tagId) {
+//     matchingItems.innerHTML += '<li class="json-tag-match animated fadeInUp"><a  style=\"background-image:linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url(/assets/images/' + content[i].articleImage + ')\" href=\"' + content[i].articleUrl + '\">' + '<section><h5>' + content[i].articleTitle + '</h5><p>' + content[i].articleDescription + '</p></section></li>';
+// }
