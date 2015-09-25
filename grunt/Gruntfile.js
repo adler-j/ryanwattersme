b1302f54761861ddde7f197a1b520528c40ffa16
Ryan Watters <watters.ryan@gmail.com>
2015-09-25 16:46:06 -0500

    Improvements to includes for svg icons

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
     }
   });
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['watch']);
 };
---
+++ parse-test.md
@@ -0,0 +1,12 @@
+---
+layout: parse
+insearch: false
+title: Working With Parse's Javascript Library
+insearch: false 
+permalink: /parse/
+---
+
+### This is just a parse testing page
+
+
+#### {{page.permalink}}
