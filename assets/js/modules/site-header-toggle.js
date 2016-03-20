2bc8497c03df26302f91c61b292640121b90bc36
Ryan Watters <watters.ryan@gmail.com>
2016-03-20 04:15:04 -0500

    small changes to homepage copy cancel button menu

--- assets/js/modules/site-header-toggle.js
+++ assets/js/modules/site-header-toggle.js
@@ -5,9 +5,17 @@ if ('ontouchstart' in document.documentElement) {
     clickOrTouchStart = "click";
 }
 var headerToggle = document.getElementById('site-header-toggle');
+var headerCancel = document.getElementById('close-menu');
 var mainContent = document.querySelector('main');
 mainContent.addEventListener(clickOrTouchStart, toggleHeader, false);
 headerToggle.addEventListener(clickOrTouchStart, toggleHeader, false);
+headerCancel.onclick = function(event) {
+    event.preventDefault();
+    document.querySelector('body > header').classList.remove('open');
+    document.querySelector('main').classList.remove('open');
+    document.querySelector('body > footer').classList.remove('open');
+}
+
 
 function toggleHeader(event) {
     var header = document.querySelector('body > header');
