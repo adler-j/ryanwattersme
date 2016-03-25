b07da8c80ad3f2a4eeba24fdc0e3a75557fdb5b1
Ryan Watters <watters.ryan@gmail.com>
2016-03-25 17:50:45 -0500

    changes to crp jq and pspeed insights

--- assets/js/modules/site-header-toggle.js
+++ assets/js/modules/site-header-toggle.js
@@ -1,35 +1,35 @@
-var clickOrTouchStart;
-if ('ontouchstart' in document.documentElement) {
-    clickOrTouchStart = "touchstart";
-} else {
-    clickOrTouchStart = "click";
-}
-var headerToggle = document.getElementById('site-header-toggle');
-var headerCancel = document.getElementById('close-menu');
-var mainContent = document.querySelector('main');
-mainContent.addEventListener(clickOrTouchStart, toggleHeader, false);
-headerToggle.addEventListener(clickOrTouchStart, toggleHeader, false);
-headerCancel.onclick = function(event) {
-    event.preventDefault();
-    document.querySelector('body > header').classList.remove('open');
-    document.querySelector('main').classList.remove('open');
-    document.querySelector('body > footer').classList.remove('open');
-}
+// var clickOrTouchStart;
+// if ('ontouchstart' in document.documentElement) {
+//     clickOrTouchStart = "touchstart";
+// } else {
+//     clickOrTouchStart = "click";
+// }
+// var headerToggle = document.getElementById('site-header-toggle');
+// var headerCancel = document.getElementById('close-menu');
+// var mainContent = document.querySelector('main');
+// mainContent.addEventListener(clickOrTouchStart, toggleHeader, false);
+// headerToggle.addEventListener(clickOrTouchStart, toggleHeader, false);
+// headerCancel.onclick = function(event) {
+//     event.preventDefault();
+//     document.querySelector('body > header').classList.remove('open');
+//     document.querySelector('main').classList.remove('open');
+//     document.querySelector('body > footer').classList.remove('open');
+// }
 
 
-function toggleHeader(event) {
-    var header = document.querySelector('body > header');
-    var mainContent = document.querySelector('main');
-    if (header.classList.contains('open')) {
-        event.preventDefault();
-        document.querySelector('body > header').classList.remove('open');
-        document.querySelector('main').classList.remove('open');
-        document.querySelector('body > footer').classList.remove('open');
-    } else if ((event.target.id === "site-header-toggle") && !(header.classList.contains('open'))) {
-        event.preventDefault();
-        document.querySelector('body > header').classList.add('open');
-        document.querySelector('main').classList.add('open');
-        document.querySelector('body > footer').classList.add('open');
-    }
+// function toggleHeader(event) {
+//     var header = document.querySelector('body > header');
+//     var mainContent = document.querySelector('main');
+//     if (header.classList.contains('open')) {
+//         event.preventDefault();
+//         document.querySelector('body > header').classList.remove('open');
+//         document.querySelector('main').classList.remove('open');
+//         document.querySelector('body > footer').classList.remove('open');
+//     } else if ((event.target.id === "site-header-toggle") && !(header.classList.contains('open'))) {
+//         event.preventDefault();
+//         document.querySelector('body > header').classList.add('open');
+//         document.querySelector('main').classList.add('open');
+//         document.querySelector('body > footer').classList.add('open');
+//     }
 
-}
+// }
