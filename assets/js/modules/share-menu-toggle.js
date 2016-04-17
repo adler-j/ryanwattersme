82993fede83b46e1ae011579c0e617d4fc5bbc20
Ryan Watters <watters.ryan@gmail.com>
2016-04-17 02:30:28 -0500

    added category dropdown for blog in main menu

--- assets/js/modules/share-menu-toggle.js
+++ assets/js/modules/share-menu-toggle.js
@@ -1,8 +1,18 @@
 var shareButton = document.getElementById('share-button');
-shareButton.onclick = toggleShare;
+if (shareButton) {
+    shareButton.onclick = toggleShare;
+}
 
 function toggleShare(event) {
-	event.preventDefault();
-	document.querySelector('.share-menu').classList.toggle('share-open');
-	document.getElementById('share-button').classList.toggle('share-open');
+    event.preventDefault();
+    document.querySelector('.share-menu').classList.toggle('share-open');
+    document.getElementById('share-button').classList.toggle('share-open');
+}
+
+var catToggle = document.querySelector('#categories-toggle');
+catToggle.onclick = showCategories;
+
+function showCategories() {
+	document.querySelector('.blog-categories').classList.toggle('show-categories');
+	document.getElementById('categories-toggle').classList.toggle('categories-open');
 }
